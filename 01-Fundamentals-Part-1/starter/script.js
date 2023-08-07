595af46d99afa91d5f16528e8c2fc3e38c52dcd5
miral-weybee <miral@weybee.in>
2023-08-07 09:34:59 +0530

    fundamentals part 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+// alert("Welcome to js");
+// console.log(35+36+46);
+
+// LECTURE: Values and Variables
+
+let country = 'India';
+let continent = 'Asia';
+let population = 8000000000
+
+console.log('Country:'+country);
+console.log('Continent:'+continent);
+console.log('Population:'+population);
+
+// LECTURE: Data Types
+let isIsland = false;
+let language;
+console.log('Type of isIsland:'+typeof(isIsland));
+console.log('Type of population:'+typeof(population));
+console.log('Type of country:'+typeof(country));
+console.log('Type of language:'+typeof(language));
+
+// LECTURE: let, const and var
+language = 'Gujarati';
+const PI = 3.14
