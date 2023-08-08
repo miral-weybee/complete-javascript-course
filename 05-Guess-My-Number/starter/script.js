368ef08792dd8c5441c95d142f40f526a101c613
miral-weybee <miral@weybee.in>
2023-08-08 19:00:24 +0530

    dom started

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!'
+
+document.querySelector('.check').addEventListener(
+    'click', function () {
+        const guess = Number(document.querySelector('.guess').value)
+        if(!guess){
+            document.querySelector('.message').textContent = 'No Number'
+        }
+    });
